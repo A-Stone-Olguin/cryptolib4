73d5e516d5ad2debc100bac64da8ecd1c64b6d4b
Stone Olguin <olguinstone@gmail.com>
2023-07-14 11:47:10 -0600

    Casting issue. Then it is finished

--- Cryptolib4/Negligible.lean
+++ Cryptolib4/Negligible.lean
@@ -68,46 +68,109 @@ lemma zero_negl : negligible (λ _ => 0) := by
 
 #check Nat.cast_le.mpr
 
+#check lt_of_lt_of_le
+
+example (n m x: ℝ) (hnm : n < m) (hmx : m < x) : n < x := by exact gt_trans hmx hnm
+-- example (n m x: ℝ) (hnm : n < m) (hmx : m < x) : c > 0 → m * c = n *c → m= n  := by apply?
+example (n m x y: ℝ) (hnm : n ≤ m ) :n * x ≤ m * x := by refine Iff.mpr (mul_le_mul_right ?a0) hnm 
+example (n : ℕ) (c : ℕ) (hc : c > 0) (hn : n > 0) (hcn0 : c ≠ 0): n^(c+1) = n * n^c := by exact Nat.pow_succ'
+example (n : ℝ) (c : ℝ) (hn : n > 0) : n^(c+1) = n^c * n := by
+  apply Real.rpow_add_one (ne_of_gt hn)
+
+example (n : ℝ) (c : ℝ) (hn : n ≥ 2) : n^(c+1) = n^c * n := by
+  apply Real.rpow_add_one (ne_of_gt (lt_of_lt_of_le  two_pos hn))
+
+   
+
+
+example (n c : ℝ) (hn : n > 0) (hc : c > 0): 1/n = 1/c → n = c :=by 
+  intro h 
+  simp at h
+  exact h
+
+example (n c : ℝ) (hn : n > 0) (hc : c > 0): n=c → 1/n = 1/c :=by 
+  intro h
+  simp 
+  exact h
+
+example (n : ℝ) (c : ℝ) (hn : n > 0) : 1/n^(c+1) = 1/n^c * 1/n^1 := by
+  ring_nf
+  apply Real.rpow_neg
+
+example (n : ℝ) (hn :n > 0) (hpos :  0 < (n^c)): 0 < (n^c)⁻¹  := by exact Iff.mpr inv_pos hpos
+
+#check Real.rpow_add
+#check Real.rpow_neg
+#check one_div_pow
+#check Real.mul_pos
+#check Real.rpow_inv_le_iff_of_neg
+#check Real.rpow_pos_of_pos
+#check Nat.cast_div
+
 lemma negl_add_negl_negl {f g : ℕ → ℝ} : negligible f → negligible g → negligible (f + g) := by 
-  intro hf hg c hc
-  have hc1 : 0 < (c + 1)  := add_pos hc one_pos
-  have hf2 := hf (c+1) hc1
-  have hg2 := hf (c+1) hc1
-  cases hf2 with
-  | intro nf hnf => 
-  cases hg2 with
-  | intro ng hng => 
-  let n₀ := max (max nf ng) 2
+  intro hf hg c hc 
+  have hc1 : 0 < (c + 1) := add_pos hc one_pos
+  cases (hf (c + 1) hc1) with
+  | intro nf hnf =>
+  cases (hg (c + 1) hc1) with
+  | intro ng hng =>
+  let n₀ := max (max nf ng) 2 
   use n₀ 
-  intros n hn 
-  let nr := (n : ℝ)
-  have n_eq_nr : (n : ℝ) = nr := rfl 
-
+  intro n hn
   have tn : max nf ng ≤ n₀ := le_max_left (max nf ng) 2 
-  have t2n₀ : 2 ≤ n₀ := le_max_right (max nf ng) 2 
-  have t2n : 2 ≤ n := by linarith 
-  have t2nr : 2 ≤ nr := by 
-    rw [← n_eq_nr]
-    exact Nat.cast_le.mpr t2n
-  have tnr_pos : 0 < nr := by linarith 
-
-  have t2na : (1 / nr) * (1/ nr^c) ≤ (1 / (2 : ℝ)) * (1 / nr^c) := by
-    have ht2 : 0 < (1 / nr^c) := by apply one_div_pos.mpr; exact Real.rpow_pos_of_pos tnr_pos c
-    apply (mul_le_mul_right ht2).mpr 
-    exact Iff.mpr (one_div_le_one_div tnr_pos two_pos) t2nr
-  have tnr2 : 1 / nr^(c+1) ≤(1 / (2: ℝ)) * (1 / nr^c) := by 
-    sorry 
-  have tnf : nf ≤ n := by 
+  have h1 : nf ≤ n := by 
+    have h := le_max_left nf ng 
+    trans 
+    exact h
+    trans
+    exact tn 
+    exact hn 
+  have h2 : ng ≤ n := by
+    have h := le_max_right nf ng 
+    trans 
+    exact h
+    trans
+    exact tn 
+    exact hn 
+  specialize hnf n h1 
+  specialize hng n h2 
+  have h_add : abs (f n) + abs (g n) < 2 / ↑n^(c + 1) := calc
+    abs (f n) + abs (g n) < 1 / ↑n ^ (c + 1) + 1 / ↑n ^ (c + 1) := by exact add_lt_add hnf hng
+                        _ = 2 / ↑n ^ (c + 1) := by ring_nf
+  have h_fg_add :abs ((f + g) n) = abs (f n + g n) := by exact rfl
+  rw [h_fg_add]
+  apply gt_of_gt_of_ge _ (abs_add (f n) (g n))
+  apply gt_of_ge_of_gt _ h_add
+  have h : n ≥ 2 := by 
+    apply le_trans _ hn 
+    apply le_max_right 
+  have hlt :  1/n ≤ 1/2 := by refine Nat.div_le_div_left h two_pos 
+  have hltr : 1 / (n: ℝ) ≤ 1 / 2  := by
+    
     sorry
-  have tfn := hnf n tnf
-  have tf : abs (f n) < (1 / (2 : ℝ)) * (1 / nr^c) := by linarith 
+  have hnc : n^(c+1) = n^c * n := by 
+    apply Real.rpow_add_one
+    apply ne_of_gt 
+    norm_cast 
+    exact lt_of_lt_of_le two_pos h
+  have h2pos :  0 < 2 / ↑n ^ c := by 
+    have hinv : 2/(n^c) = 2 * (n^c)⁻¹ := rfl 
+    rw [hinv]
+    apply Real.mul_pos two_pos 
+    apply (inv_pos).mpr 
+    apply Real.rpow_pos_of_pos
+    apply lt_of_lt_of_le two_pos
+    norm_cast
+  have hltc2 : 2/n^(c+1) ≤ 1/n^c := calc 
+    2 / n^(c+1) = 2 / (n^c * n) := by rw [hnc]
+              _ = 2 / n^c * 1 / n  := by ring_nf
+              _ ≤ 2 / n^c * 1/2 := by apply Iff.mpr (mul_le_mul_left _); simp at hltr; exact hltr; simp; exact h2pos
+              _ = 1 / n^c := by ring_nf
 
-  have tng : ng ≤ n := by 
-    sorry 
-  have tgn := hng n tng 
-  have tg : abs (g n) < (1 / (2 : ℝ)) * (1 /nr^c) := by sorry 
-
-  sorry
+  have goal : 1 / ↑n ^ c ≥ 2 / ↑n ^ (c + 1) := by 
+    apply le_trans hltc2 
+    rfl
+  exact goal
 
 
 
@@ -130,9 +193,6 @@ lemma nat_mul_negl_negl {f : ℕ → ℝ} (m : ℕ) : negligible f → negligibl
       rfl
     rw [d]
     exact negl_add_negl_negl ih hf
-
-example (n k c: ℝ) (h : abs n ≤ abs k  ) : abs (n *c) ≤ abs (k * c) := by 
-  apply abs_le_abs
 
 lemma const_mul_negl_negl  {f : ℕ → ℝ} (m : ℝ) : negligible f → negligible (λ n => m * (f n)) := by 
   intro hf 
