8787fc4d7501383b037fa29428741c5e8adbff08
Stone Olguin <olguinstone@gmail.com>
2023-07-18 11:53:29 -0600

    One lemma done. Need to find simp lemmas for Pmf

--- Cryptolib4/Uniform.lean
+++ Cryptolib4/Uniform.lean
@@ -25,25 +25,28 @@ instance : Fact (0 < 2) where
 def uniform_zmod (n : ℕ) [Fact (0 < n)] [NeZero n] : Pmf (ZMod n) := uniform_group (ZMod n)
 def uniform_2 : Pmf (ZMod 2) := uniform_zmod 2 
 
-#check congr_fun
-
+-- Had to modify from NNReal to ENNReal
 lemma uniform_group_prob : 
   ∀ (g : G), (uniform_group G) g = 1 / Multiset.card (@Fintype.elems G).val := by 
   intro g 
   
-  have h1 : ↑(uniform_group G) = (λ (a : G) => 
-    (Multiset.count a (@Fintype.elems G).val : NNReal) / Multiset.card (@Fintype.elems G).val) := by 
-    
+  have h1 : (uniform_group G)= (λ (a : G) => 
+    (Multiset.count a (@Fintype.elems G).val : ENNReal) / Multiset.card (@Fintype.elems G).val) := by 
+    ext
+    simp [uniform_group, Pmf.ofMultiset]
     sorry
   have h2 : (uniform_group G) g = 
-    Multiset.count g (@Fintype.elems G).val / Multiset.card (@Fintype.elems G).val := by 
-      -- exact congr_fun h1 g
-      sorry
-    
+    Multiset.count g (@Fintype.elems G).val / Multiset.card (@Fintype.elems G).val := congr_fun h1 g
   rw [h2]
   have h3 : Multiset.count g (@Fintype.elems G).val = 1 := Multiset.count_univ g 
   rw [h3]
   simp
 
-  -- TODO need to add instance of uniform group for ZMod
-
+lemma uniform_zmod_prob {n : ℕ} [Fact (0 < n)] [NeZero n] : ∀ (a : ZMod n), (uniform_zmod n) a = 1/n := by
+  intro a 
+  simp [uniform_zmod]
+  have h1 := uniform_group_prob (ZMod n) a
+  have h2 : Multiset.card (@Fintype.elems (ZMod n)).val = n := ZMod.card n
+  rw [h2] at h1
+  rw [inv_eq_one_div]
+  exact h1 
