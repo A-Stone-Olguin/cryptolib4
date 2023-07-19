60fe3231b13082131316210196c6761372c1ceda
Stone Olguin <87540812+A-Stone-Olguin@users.noreply.github.com>
2023-07-19 11:49:17 -0600

    Decisional Diffie-Hellman Finished (#9)
    * Added Decisional Diffie-Hellman definition
    
    * Added Local notations
    
    * Notation and DDH. g_gen_G and G_card_q removed

---
+++ Cryptolib4/DDH.lean
@@ -0,0 +1,45 @@
+/-
+ -----------------------------------------------------------
+  The Decisional Diffie-Hellman assumption as a security game
+ -----------------------------------------------------------
+-/
+
+
+import Mathlib.Probability.ProbabilityMassFunction.Basic
+import Mathlib.Probability.ProbabilityMassFunction.Monad
+import Mathlib.Probability.ProbabilityMassFunction.Constructions
+import Cryptolib4.ToMathlib
+import Cryptolib4.Uniform
+
+noncomputable section
+
+section DDH
+
+variable (G : Type) [Fintype G] [Group G]
+          (g : G) (g_gen_G : ∀ (x : G), x ∈ Subgroup.zpowers g) 
+          (q : ℕ) [Fact (0 < q)] [NeZero q] (G_card_q : Fintype.card G = q) 
+          (D : G → G → G → Pmf (ZMod 2))
+
+def DDH0 : Pmf (ZMod 2) := 
+  do
+    let x ← uniform_zmod q
+    let y ← uniform_zmod q 
+    let b ← D (g^x.val) (g^y.val) (g^(x.val*y.val))
+    pure b
+
+def DDH1 : Pmf (ZMod 2) := 
+  do
+    let x ← uniform_zmod q
+    let y ← uniform_zmod q 
+    let z ← uniform_zmod q 
+    let b ← D (g^x.val) (g^y.val) (g^z.val)
+    pure b
+
+-- -- From Lupo: DDH0(D) is the event that D outputs 1 upon receiving (g^x, g^y, g^(xy))
+local notation "Pr[DDH0("D")]" => DDH0 G g q D 1
+-- -- From Lupo: DDH1(D) is the event that D outputs 1 upon receiving (g^x, g^y, g^z)
+local notation "Pr[DDH1("D")]" => DDH1 G g q D 1
+
+def DDH (ε : ENNReal) : Prop := abs (Pr[DDH0(D)] - Pr[DDH1(D)]) ≤ ε
+
+end DDH
